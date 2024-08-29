49b777c9c7352b38bb1cf3d8f7c5ae6f7d588cc8
breadbun407 <121478122+breadbun407@users.noreply.github.com>
2024-08-29 14:13:08 +0800

    sPOTIFY WEB PLAYER
    Added!@

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,18 +1,23 @@
-/* eslint-disable no-unused-vars */
-// components/Home.js
-import React from 'react';
-
 function Home() {
     return (
         <div className="container">
             <h1>Welcome to My Music</h1>
             <section>
                 <h2>Latest Singles</h2>
-                {/* Add your singles list here */}
+                <div>Check out my latest song, Breaking Point, available on all streaming platforms!</div>
             </section>
             <section>
                 <h2>Spotify Player</h2>
-                {/* Add Spotify embed code here */}
+                <iframe
+                    style={{ borderRadius: '12px' }}
+                    src="https://open.spotify.com/embed/artist/66PPP1Zp5cTWEerszKBlDs?utm_source=generator"
+                    width="100%"
+                    height="352"
+                    frameBorder="0"
+                    allowFullScreen
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                    loading="lazy">
+                </iframe>
             </section>
             <section>
                 <h2>Twitter Feed</h2>
